b2fa9e85685c0491e627aa404bef875f1f37b59c
Alesson Marques <alessonmarques@live.com>
2021-06-18 01:57:22 -0300

    Integrated with Coingecko to got trough those Price/ data problems with BSCScan API.

--- app.php
+++ app.php
@@ -1,14 +1,24 @@
 <?php
 
+    use Codenixsv\CoinGeckoApi\CoinGeckoClient;
     use Core\Account;
+    
+
     
     $wallet     = '0xe99831A2bD01C812fd91038B03FE2f09dAA9F0A7';
     $account    = new Account($wallet);
 
-    echo "<pre>";
-    //print_r($account->getNormalTXList()->result[81]);
-    print_r($account->getInternalTX('0x9e88e3914ddeabf3d8f76e5b27a38dd4872ec08e7f0043a26ffdc3944d74a57c'));
-    //print_r($account->getInternalTXList());
 
+    $gecko = new CoinGeckoClient();
+    $data = $gecko->ping();
+
+    
+    
+    echo "<pre>";
+        print_r($data);
+        //print_r($account->getNormalTXList()->result[81]);
+        print_r($account->getInternalTX('0x9e88e3914ddeabf3d8f76e5b27a38dd4872ec08e7f0043a26ffdc3944d74a57c'));
+        //print_r($account->getInternalTXList());
     echo "</pre>";
+
     die();
