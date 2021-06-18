de5c8720b0b03ce71225008b4d732944c68a5b82
Alesson Marques <alessonmarques@live.com>
2021-06-18 19:13:56 -0300

    Changing the bootstrap test to the index to free the setup file of it.

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php 
+    // Just to test moment.
+    if (!$_GET['wallet'])
+    {
+        require __DIR__ . "/HebernMachine.php";
+        die();
+    }
 
+    $debug = $_GET['debug'];
+
+    if(!$debug)
+    {
+        header("Access-Control-Allow-Origin: *");
+        header('Content-Type: application/json; charset=utf-8');
+        header("Cache-Control: no-cache, no-store, must-revalidate");
+    }
+    
     /**
      * Start the autoloader to bring all classes to the system.
      */
