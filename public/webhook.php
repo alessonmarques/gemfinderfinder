e0c71044397f7a47fdb56e4635c909082039f9f1
Alesson Marques <alessonmarques@live.com>
2021-06-18 20:39:11 -0300

    Adding webhook log to find the requests.

--- public/webhook.php
+++ public/webhook.php
@@ -1,6 +1,13 @@
 <?php
 
     /**
+    * Defines the log to get access data.
+    */
+    
+    $SERVER_REQUEST_DATA = json_encode($_SERVER);
+    error_log($SERVER_REQUEST_DATA);
+
+    /**
     * Start the autoloader to bring all classes to the system.
     */
     require __DIR__ . '/../bootstrap.php';
